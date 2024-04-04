4fa96da93d7f5febd0a26764fed87a14114b61da
Enrique Saínz-Terrones Peña <6258@CALASANZSALAMANCA.local>
2024-04-04 11:03:17 +0200

    pruebas con variables y constantes

---
+++ Prueba/src/Constantes.java
@@ -0,0 +1,6 @@
+
+public class Constantes {
+	static final int GRADOS_CONGELACION_FARENHEIT = 32;
+	static final float CORRECCIÓN_DE_CRECIMIENTO = 5 / 9f;
+
+}
--- Prueba/src/Principal.java
+++ Prueba/src/Principal.java
@@ -3,7 +3,17 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("HolaMundo");
+		System.out.println("HolaMundo");
+		int miVariable = 50;
+		System.out.println(miVariable);
+		++miVariable;
+		System.out.println(miVariable);
+		miVariable = miVariable + 1;
+		System.out.println(miVariable);
+		float gradosfarenheit = 98.6f;
+		float gradosCentígrados = (gradosfarenheit - Constantes.GRADOS_CONGELACION_FARENHEIT)
+				* Constantes.CORRECCIÓN_DE_CRECIMIENTO;
+
 	}
 
 }
